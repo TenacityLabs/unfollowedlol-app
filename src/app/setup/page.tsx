69670c432f2b96062f890143e585bb7f1a022664
edisonqu <edisonquu@gmail.com>
2024-04-28 01:50:17 -0400

    change iframe url

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
       <iframe
-        src="https://v2-embednotion.com/c02a5cfa6b544585b201d40160c08367"
+        src="https://v2-embednotion.com/c75befe915b34e6091f23124b7bc97df"
         style={{width: '100%', height: '1000px', border: '2px solid #ccc', borderRadius: '10px', padding: 'none'}}
       ></iframe>
     );
