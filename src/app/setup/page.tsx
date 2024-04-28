329abd2bf3278318f189b9eb1a7bb71cd3f7c447
edisonqu <edisonquu@gmail.com>
2024-04-28 01:29:30 -0400

    add setup page

---
+++ src/app/setup/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+      <iframe
+        src="https://v2-embednotion.com/c02a5cfa6b544585b201d40160c08367"
+        style={{width: '100%', height: '1000px', border: '2px solid #ccc', borderRadius: '10px', padding: 'none'}}
+      ></iframe>
+    );
+  }
