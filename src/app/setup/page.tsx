13ebd7fba19bb4f3c2fe9edf7b044935ad2a1e36
edisonqu <edisonquu@gmail.com>
2024-07-07 15:30:31 -0400

    update iframe and privacy

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -1,8 +1,8 @@
 export default function Page() {
-    return (
-      <iframe
-        src="https://v2-embednotion.com/c75befe915b34e6091f23124b7bc97df"
-        style={{width: '100%', height: '1000px', border: '2px solid #ccc', borderRadius: '10px', padding: 'none'}}
+  return (
+    <iframe
+      src="https://v2-embednotion.com/f050e0154cd84f16ad6868aee7b5b5bd"
+      style={{width: '100%', height: '1000px', border: '2px solid #ccc', borderRadius: '10px', padding: 'none'}}
       ></iframe>
-    );
-  }
+  );
+}
