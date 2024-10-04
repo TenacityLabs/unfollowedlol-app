f5c1f97472a0cdb4d292f1fa2c5a4e6422d551e1
edisonqu <edisonquu@gmail.com>
2024-10-03 22:53:46 -0400

    add unoptimzied

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+      unoptimized: true,
       remotePatterns: [
         {
           protocol: "https",
-          unoptimized: true,
           hostname: "**",
         },
       ],
